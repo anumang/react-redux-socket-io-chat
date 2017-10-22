1387ab690cfe1b60ab1ba5f625183aeee622c850
Alper Numan Genc <anumangenc@gmail.com>
2017-10-22 16:19:01 +0300

    Missing CORS config fixed.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const init = (store) => new Promise((resolve, reject) => {
   try {
     //CORS middleware
     const allowCrossDomain = (req, res, next) => {
-      res.header('Access-Control-Allow-Origin', env.HOST_URL);
+      res.header('Access-Control-Allow-Origin', env.HOST_URL + ':' + env.HOST_PORT);
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type');
       res.header('Access-Control-Allow-Credentials', 'true');
